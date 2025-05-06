fbeafda7f2456cafeb85ef43dad0ca6ef1db81b4
Jan Rehanek <jan.rehanek@rockwellautomation.com>
2025-05-06 13:41:21 +0200

    init commit

---
+++ app_sender/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	port = "54321"
+)
+
+var (
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: false}))
+)
+
+func main() {
+	logger.Info("Application starting. Creating server and router.", slog.String("port", port))
+
+	mux := &http.ServeMux{}
+	mux.Handle("GET /api/v1/weather", http.HandlerFunc(weatherHandler))
+	mux.Handle("/live", http.HandlerFunc(probeHandler))
+	mux.Handle("/ready", http.HandlerFunc(probeHandler))
+
+	server := http.Server{
+		Addr:           ":" + port,
+		Handler:        mux,
+		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	logger.Info("Application has started successfully. Server is listening.", slog.String("port", port))
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			logger.Error("Server error. Application shutting down.", slog.String("err", err.Error()))
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	gotSignal := <-quit
+	signal.Stop(quit)
+
+	logger.Info("Termination signal received. Application shutting down.", slog.String("signal", gotSignal.String()))
+}
