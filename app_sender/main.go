e8e60ddf99059ba4446acaef223c8573cf03fd18
Jan Rehanek <jan.rehanek@rockwellautomation.com>
2025-05-20 14:27:38 +0200

    Created app_sender app with Dockerfile. Moved Helm chart.

--- app_sender/main.go
+++ app_sender/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"log/slog"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 const (
-	port = "54321"
+	targetServiceEnvName = "TARGET_SERVICE"
 )
 
 var (
@@ -18,29 +16,17 @@ var (
 )
 
 func main() {
-	logger.Info("Application starting. Creating server and router.", slog.String("port", port))
-
-	mux := &http.ServeMux{}
-	mux.Handle("GET /api/v1/weather", http.HandlerFunc(weatherHandler))
-	mux.Handle("/live", http.HandlerFunc(probeHandler))
-	mux.Handle("/ready", http.HandlerFunc(probeHandler))
-
-	server := http.Server{
-		Addr:           ":" + port,
-		Handler:        mux,
-		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+	targetService, ok := os.LookupEnv(targetServiceEnvName)
+	if !ok {
+		logger.Error("Target service could not be retrieved from env. Shutting down.", slog.String("envName", targetServiceEnvName))
+		os.Exit(1)
 	}
 
-	logger.Info("Application has started successfully. Server is listening.", slog.String("port", port))
+	logger.Info("Application starting. Creating request emitter.")
 
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			logger.Error("Server error. Application shutting down.", slog.String("err", err.Error()))
-			os.Exit(1)
-		}
-	}()
+	go handleProbes()
+
+	go emit(targetService)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
--- app_sender/utils.go
+++ app_sender/utils.go
@@ -4,28 +4,90 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"os"
+	"time"
 )
+
+const targetEndpoint = "/api/v1/weather"
+
+func emit(targetService string) {
+	url := targetService + targetEndpoint
+
+	counter := 1
+	for {
+		func() {
+			defer func() {
+				counter++
+				time.Sleep(time.Second * 5)
+			}()
+
+			reqLogger := logger.With(slog.Int("requestNumber", counter))
+
+			req, err := http.NewRequest(http.MethodGet, url, nil)
+			if err != nil {
+				panic("request is malformed: " + err.Error())
+			}
+
+			req.Header.Set("User-Agent", "Workshop_App_Sender")
+
+			reqLogger.Info("Sending HTTP request.", slog.String("targetUrl", url))
+
+			client := &http.Client{
+				Timeout: time.Second * 10,
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				reqLogger.Error("Error received while sending request.", slog.String("err", err.Error()), slog.String("url", url))
+				return
+			}
+
+			if resp.StatusCode != 200 {
+				reqLogger.Error("Request sent. Received unexpected response status code.",
+					slog.String("statusCode", resp.Status),
+				)
+				return
+			}
+
+			defer resp.Body.Close()
+
+			var respBody map[string]string
+			err = json.NewDecoder(resp.Body).Decode(&respBody)
+			if err != nil {
+				reqLogger.Error("Request sent. Failed to decode response body.")
+				return
+			}
+
+			reqLogger.Info("Request sent. Received positive response.",
+				slog.String("statusCode", resp.Status),
+				slog.String("weatherReport", respBody["weather"]),
+			)
+		}()
+	}
+}
+
+func handleProbes() {
+	mux := &http.ServeMux{}
+	mux.Handle("/live", http.HandlerFunc(probeHandler))
+	mux.Handle("/ready", http.HandlerFunc(probeHandler))
+
+	server := http.Server{
+		Addr:           ":54321",
+		Handler:        mux,
+		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			os.Exit(1)
+		}
+	}()
+}
 
 func probeHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte(getHelloWorldString()))
-}
-
-func weatherHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	resp := map[string]string{
-		"weather": "cloudy",
-	}
-
-	bs, err := json.Marshal(resp)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	logger.Info("Request received. Sending positive response.", slog.String("uri", r.RequestURI), slog.String("method", r.Method))
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(bs)
 }
 
 // getHelloWorldString only exists so that we can have a functional unit test for our pipeline.
